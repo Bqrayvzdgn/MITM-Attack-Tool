683fc1af455cc2bd40018f7e2a350590747466ee
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-21 14:18:25 +0300

    Update mitm-attacker.py

--- mitm-attacker.py
+++ mitm-attacker.py
@@ -70,9 +70,9 @@ if __name__ == "__main__":
             arp_poisoning(poisonIP, targetIP)
             """
             number += 1
-            print(f"\rPackages are being sent from address {user_target_ip} to address {user_gateway_ip}. " + str(number), end="")
-            print(f"\rPackages are being sent from address {user_gateway_ip} to address {user_target_ip}. " + str(number), end="")
+            print(f"\rPackages are being sent from address {user_target_ip} to address {user_gateway_ip}. ", end="")
             time.sleep(3)
+            print(str(number))
     except KeyboardInterrupt:
         print("\n\nEverything is being restored to its former state.")
         """
